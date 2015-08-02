ac3dcf84cde17af6b3aa6f0ff94c4730bce662bc
manuraj17 <dev.manu@yahoo.com>
2015-08-02 23:09:19 +0530

    First Commit

--- index.js
+++ index.js
@@ -6,15 +6,13 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+app.set('views', __dirname + '/templates');
+app.set('view engine', 'jade');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+  response.render('index');
 });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
