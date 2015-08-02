16184fbb005a3b1b9d2d3053d57355d3dca0c470
manuraj17 <dev.manu@yahoo.com>
2015-08-03 01:22:05 +0530

    More Bootstrap

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ app.get('/', function(request, response) {
   response.render('index');
 });
 
+app.get('/newPost', function(request, response) {
+  response.render('newPost');
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
